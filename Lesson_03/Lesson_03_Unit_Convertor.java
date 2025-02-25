07ef2ed5e5a89145ebe586bab7cde56148dec989
svetlanacibula <71784185+svetlanacibula@users.noreply.github.com>
2025-02-25 14:52:58 +0200

    Update Lesson_03_Unit_Convertor.java

--- Lesson_03/Lesson_03_Unit_Convertor.java
+++ Lesson_03/Lesson_03_Unit_Convertor.java
@@ -2,13 +2,23 @@ public class Lesson_03_Unit_Convertor {
   public static void main(String[] args) {
 
     int heightInInches = 5;
-    double heightInCentimeters = convertInchToCentimeters(heightInInches);
-    System.out.println("Height in centimeters is " + heightInCentimeters + "cm");
+    int heightInFeet = 3;
+    double heightInCentimetersMethod1 = convertInchToCentimeters(heightInInches);
+    double heightInCentimetersMethod2 = convertFeetAndInchesToCentimeters(heightInFeet, heightInInches);
+
+
+    System.out.println("Height in centimeters is " + heightInCentimetersMethod1 + "cm");
+    System.out.println("Height in inches is " + heightInCentimetersMethod2 + "cm");
   }
 
     public static double convertInchToCentimeters(int inches) {
       double centimeterPerInch = 2.54;
       return inches * centimeterPerInch;
+    }
+
+    public static double convertFeetAndInchesToCentimeters(int feet, int inches) {
+    int totalInches = (feet * 12) + inches;
+    return convertInchToCentimeters(totalInches);
     }
 
 }
